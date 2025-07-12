44138bc89a0cb7bf6334a8ae649b68ff7a2b783a
rogoma007 <rogoma007@gmail.com>
2025-07-12 16:31:01 -0300

    Ajustes casa 12-07-2025 1600

--- resources/views/contract/contracts/show.blade.php
+++ resources/views/contract/contracts/show.blade.php
@@ -868,18 +868,17 @@
 
                                             <div class="tab-pane" id="tab4" role="tabpanel">
                                                 <label class="col-form-label f-w-600">Reportes:</label>
-                                                {{-- <table id="forms" class="table table-striped table-bordered">
-                                                    <div class="container">
-                                                        <h2 class="mb-4 text-center">Conectividad Alcantarillado</h2>
-                                                        <div style="position: relative; padding-bottom: 75%; height: 0; overflow: hidden;">
-                                                            <iframe 
-                                                                src="https://docs.google.com/spreadsheets/d/14kAe0AQ693oNBuumkVBEUTFeUs9WhJtfhRKtpMh9hl8/preview?gid=0&single=true"
-                                                                style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: 0;" 
-                                                                allowfullscreen>
-                                                            </iframe>
-                                                        </div>
+
+                                                {{-- <div class="container">
+                                                    <h2 class="mb-4 text-center">Conectividad Alcantarillado</h2>
+                                                    <div style="position: relative; padding-bottom: 95%; height: 0; overflow: hidden;">
+                                                        <iframe 
+                                                            width="1000" 
+                                                            height="1500" 
+                                                            src="https://lookerstudio.google.com/embed/reporting/a390cf42-32c9-4097-89a3-b8aed4f4f6e1/page/U3qQF" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox">
+                                                        </iframe>            
                                                     </div>
-                                                </table> --}}
+                                                </div> --}}
                                             </div>
 
                                             <div class="tab-pane" id="tab3" role="tabpanel">
