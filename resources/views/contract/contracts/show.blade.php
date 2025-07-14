d02b8a39434315c1f350fa84404982340ff1464a
rogoma <rogoma007@gmail.com>
2025-07-14 13:29:24 -0300

    Ajustes 14-07-2025 oficina 1400

--- resources/views/contract/contracts/show.blade.php
+++ resources/views/contract/contracts/show.blade.php
@@ -871,13 +871,13 @@
                                                 {{-- <table id="forms" class="table table-striped table-bordered">
                                                     <div class="container">
                                                         <h2 class="mb-4 text-center">Conectividad Alcantarillado</h2>
-                                                        <div style="position: relative; padding-bottom: 75%; height: 0; overflow: hidden;">
-                                                            <iframe 
-                                                                src="https://docs.google.com/spreadsheets/d/14kAe0AQ693oNBuumkVBEUTFeUs9WhJtfhRKtpMh9hl8/preview?gid=0&single=true"
-                                                                style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: 0;" 
-                                                                allowfullscreen>
-                                                            </iframe>
-                                                        </div>
+                                                    <div style="position: relative; padding-bottom: 95%; height: 0; overflow: hidden;">
+                                                        <iframe 
+                                                            width="1000" 
+                                                            height="1500" 
+                                                            src="https://lookerstudio.google.com/embed/reporting/a390cf42-32c9-4097-89a3-b8aed4f4f6e1/page/U3qQF" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox">
+                                                        </iframe>            
+                                                    </div>
                                                     </div>
                                                 </table> --}}
                                             </div>
